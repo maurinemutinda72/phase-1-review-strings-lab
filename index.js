743024a138ab80038e18b4fa3c7d2c4489cb8624
moureen mutinda <maurinemutinda72@gmail.com>
2024-12-19 11:21:33 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+// Define the currentUser variable
+const currentUser = 'Grace Hopper';
+
+// Create a personalized welcome message using string interpolation
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Create an excited version of the welcome message by converting it to uppercase
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// Create a short greeting that includes only the first character of the current user's name
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
